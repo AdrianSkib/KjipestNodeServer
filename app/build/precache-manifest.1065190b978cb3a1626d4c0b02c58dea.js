f479b22f0840613eeaa64ece86ae36bdb2940ff9
Adrian Bogen Skibelid <ADRS@equinor.com>
2021-04-06 19:54:57 +0200

    Removed /build from gitignore

---
+++ app/build/precache-manifest.1065190b978cb3a1626d4c0b02c58dea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003cc9d48accd2e55a51819d86283178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f137da9d904a141cc0",
+    "url": "/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "0de480406bc0628c43c8",
+    "url": "/static/css/main.8036bff7.chunk.css"
+  },
+  {
+    "revision": "49f137da9d904a141cc0",
+    "url": "/static/js/2.187d4b93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.187d4b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de480406bc0628c43c8",
+    "url": "/static/js/main.e54037a4.chunk.js"
+  },
+  {
+    "revision": "68793143ab0a6c4e149f",
+    "url": "/static/js/runtime-main.2c760ad8.js"
+  },
+  {
+    "revision": "7a75bec1ada5f2fe72d077a803a7714f",
+    "url": "/static/media/grafier-display.7a75bec1.otf"
+  },
+  {
+    "revision": "9e319e2c2922714fdddb1b4ff09d526d",
+    "url": "/static/media/grafier-text.9e319e2c.otf"
+  },
+  {
+    "revision": "617bcacd42de8c7bee32e41d6cd3a0be",
+    "url": "/static/media/logo.617bcacd.svg"
+  },
+  {
+    "revision": "aa3b800fb472515550eb8bfa8eedb840",
+    "url": "/static/media/partlycloudy.aa3b800f.svg"
+  },
+  {
+    "revision": "701bc1433e59cc0589549b253527742a",
+    "url": "/static/media/rain.701bc143.svg"
+  }
+]);
